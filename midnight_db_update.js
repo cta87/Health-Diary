ffaa32002e35042294431959df41bff73ffe5fad
Cam <51349362+cta87@users.noreply.github.com>
2020-07-03 13:39:24 +1200

    Adding sqlite query function

--- midnight_db_update.js
+++ midnight_db_update.js
@@ -1,3 +1,33 @@
+// Imports
+const SQLITE3 = require('sqlite3').verbose();
+
 // function to run at midnight to update sql database with daily dairy values
 
 // sqlite code 
+// ==================== Functions for Using a Sqlite DB file =============================
+
+function queryDb(filename, sqlQuery) {
+    console.log("Querying DB file: " + filename);
+    console.log("Query String: " + sqlQuery);
+
+    return new Promise((resolve, reject) => {
+
+        const DATA_BASE = new SQLITE3.Database('./DBStore/' + filename);
+        DATA_BASE.each(sqlQuery, (err, row) => {
+            if (err) {
+                reject(err); // optional: you might choose to swallow errors.
+            } else {
+                queries.push(row); // accumulate the data
+            }
+        }, (err, n) => {
+            if (err) {
+                console.log("Query error:" + err);
+                reject(err); // optional: again, you might choose to swallow this error.
+            } else {
+                console.log('Query complete:');
+                resolve(queries); // resolve the promise
+            }
+        });
+    });
+}
+
